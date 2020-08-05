5e3ddade2d58623588e8457656a695252d3b87b8
Paul Brannan <curlypaul924@gmail.com>
2020-08-04 21:49:56 -0400

    Fix seed generation script

--- gen_seeds.sh
+++ gen_seeds.sh
@@ -2,8 +2,8 @@
 
 # https://xen.garden/2020/04/29/smrat-data.html
 
-param="../standard_presets/SMRAT2020.json"
-randoPreset="../randomizer/SMRAT2020.json"
+param="./RandomMetroidSolver/standard_presets/SMRAT2020.json"
+randoPreset="./RandomMetroidSolver/rando_presets/SMRAT2020.json"
 
 while :
 do
@@ -11,8 +11,8 @@ do
   filename="seeds/$seed.txt"
   if [ ! -f "${filename}" ]
   then
-    ../randomizer.py \
-        --param "${param} --randoPreset "${randoPreset} \
+    ./RandomMetroidSolver/randomizer.py \
+        --param "${param}" --randoPreset "${randoPreset}" \
         --seed $seed --output /dev/null --debug --runtime 90 \
         | grep -v "DEBUG\|Apply\|DIAG" > "${filename}"
     if [ ! -s "${filename}" ]
