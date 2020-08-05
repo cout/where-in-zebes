3bd95a3078614d3fe8c4c2f0ca2b31c9927eec7d
Paul Brannan <curlypaul924@gmail.com>
2020-08-05 13:52:13 -0400

    Only use "good" items when filtering out seeds

--- engine.py
+++ engine.py
@@ -102,7 +102,7 @@ class Engine(object):
 
   def _viable_seed(self, seed):
     for location, item in seed.locations:
-      if item.found and item.found_in is not None and item.found_in != location.area:
+      if item.good and item.found and item.found_in is not None and item.found_in != location.area:
         return False
 
     return True
